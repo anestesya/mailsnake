4939a98ebafe32381b1807fac053bafa7829eeb5
Tadeu Luis Pires Gaudio <anestesya@gmail.com>
2015-07-29 15:21:26 -0300

    Update mailsnake.py
    this add first tests fo 3.0 calls and moving call to REST type

--- mailsnake/mailsnake.py
+++ mailsnake/mailsnake.py
@@ -1,42 +1,51 @@
-import urllib2
-try:
-    import simplejson as json
-except ImportError:
-    import json
+from google.appengine.api import urlfetch
+import logging
+import json
+
+mailchimp_api_url = 'https://us8.api.mailchimp.com/3.0/'
+mailchimp_api_key = 'f85e562bed97acd14553615c3d9215a7-us8'
+list_id = ''
 
 class MailSnake(object):
-    def __init__(self, apikey = '', extra_params = {}):
-        """
-            Cache API key and address.
-        """
+    """
+    MailSnake is a simple MailChimp API Wrapper.
+    - URL: https://github.com/leftium/mailsnake
+    - Author: John-Kim Murphy (https://github.com/leftium)
+    - Update By: Tadeu Luis Pires Gaudio
+    """
+    def __init__(self, apikey='', extra_params={}):
+        if not apikey:
+            raise ValueError("MailChimp API Key not valid. Set in settings.py")
+
         self.apikey = apikey
-
-        self.default_params = {'apikey':apikey}
+        self.default_params = {'apikey': apikey}
         self.default_params.update(extra_params)
+        self.base_api_url = mailchimp_api_url
 
-        dc = 'us1'
-        if '-' in self.apikey:
-            dc = self.apikey.split('-')[1]
-        self.base_api_url = 'https://%s.api.mailchimp.com/1.3/?method=' % dc
-
-    def call(self, method, params = {}):
+    def call(self, method, params={}):
         url = self.base_api_url + method
-        all_params = self.default_params.copy()
-        all_params.update(params)
+        params.update(self.default_params)
 
-        post_data = urllib2.quote(json.dumps(all_params))
-        headers = {'Content-Type': 'application/json'}
-        request = urllib2.Request(url, post_data, headers)
-        response = urllib2.urlopen(request)
+        post_data = json.dumps(params['post_data'])
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        headers['Authorization'] = 'apikey %s' % self.apikey
 
-        return json.loads(response.read())
+        result = urlfetch.fetch(url=url,
+            payload=post_data,
+            method=params['method_type'],
+            headers=headers)
+
+        return json.loads(result.content)
 
     def __getattr__(self, method_name):
-
         def get(self, *args, **kwargs):
-            params = dict((i,j) for (i,j) in enumerate(args))
+            params = dict((i, j) for (i, j) in enumerate(args))
             params.update(kwargs)
-            return self.call(method_name, params)
+            _method_name = method_name+'/'+params['id']
+            if params['resource']:
+                for resource in params['resource']:
+                    _method_name = _method_name + '/' + resource
+
+            return self.call(_method_name, params)
 
         return get.__get__(self)
-
